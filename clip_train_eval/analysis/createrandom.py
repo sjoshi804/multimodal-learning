df2c4ca59ec0a9d41e067dc838b043490a9ef317
Arnav Jain <arnavj@ucla.edu>
2023-04-18 09:49:51 -0700

    Add imagenet like data and random

--- clip_train_eval/analysis/createrandom.py
+++ clip_train_eval/analysis/createrandom.py
@@ -4,12 +4,12 @@ os.environ["WANDB_SILENT"] = "true"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3" 
 
 if(__name__ == "__main__"):   
-     with open('dsets/full_random_10.csv', 'w', encoding='UTF8', newline = '') as f:
-        f2 = open('dsets/full_clean_train_1M.csv', 'r', encoding='UTF8', newline = '') 
+     with open('dsets/imagenet_like_random1.csv', 'w', encoding='UTF8', newline = '') as f:
+        f2 = open('dsets/imagenet_like_train.csv', 'r', encoding='UTF8', newline = '') 
         lines = f2.readlines()
         allvals = list(range(1, len(lines)))
         random.shuffle(allvals)
-        for i in allvals[0: int(len(allvals) * 0.1)]:
+        for i in allvals[0: int(len(allvals) * 0.2)]:
             f.write(lines[i])
             
     
